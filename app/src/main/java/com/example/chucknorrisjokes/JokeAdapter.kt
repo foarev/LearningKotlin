03803a80897ab34cf1f506cc0ddc19202144f899
Valentin <valentin.foare@edu.esiee.fr>
2020-02-06 11:52:14 +0100

    4. Custom view for items

--- app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -1,16 +1,16 @@
 package com.example.chucknorrisjokes
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class JokeAdapter(var j:Jokes) : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>(){
-    class JokeViewHolder(val v: TextView) : RecyclerView.ViewHolder(v){
 
-    }
+class JokeAdapter(var j:Jokes) : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>(){
+    class JokeViewHolder(val v: TextView) : RecyclerView.ViewHolder(v)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        val t = TextView(parent.context)
+        val t = LayoutInflater.from(parent.context).inflate(R.layout.joke_layout, parent, false) as TextView
         return JokeViewHolder(t)
     }
 
