bf78943c51538fcc403a32e22eea73d9f478d29f
Valentin <valentin.foare@edu.esiee.fr>
2020-02-27 11:16:17 +0100

    4. Call Api to get one Joke

--- app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.*
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
@@ -21,5 +24,16 @@ class MainActivity : AppCompatActivity() {
         ad.jokes = Jokes.jokes.map { Json(JsonConfiguration.Stable).parse(Joke.serializer(), it) }
         my_recycler_view.layoutManager = llm
         my_recycler_view.adapter = ad
+
+        val joke_service:JokeApiService = JokeApiServiceFactory.factoryBuilder()
+        joke_service
+            .giveMeAJoke()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onError = { e -> Log.wtf(TAG, e) },
+                onSuccess = { joke -> Log.wtf(TAG, "$joke") }
+            )
+
     }
 }
